dbd13dc483606acc9ec38da1cef380ca7f186675
MarcosHead <marcospojr13@gmail.com>
2018-09-26 18:33:14 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -15,3 +15,28 @@ function mostraAba(idPagina, link)
 function mudarPagina(){
 	window.open("funcionarios.html", '_blank');
 }
+
+$(document).ready(function () {
+			
+		$(".fotos").each(function(i) {
+			$(this).delay(200*i).fadeIn();
+		});
+		
+		$(".fotos").hover(
+		
+			function (){
+				$(this).animate({
+					width: '220px',
+					height: '220px'
+				});
+			},
+			
+			function (){
+				$(this).animate({
+					width: '200px',
+					height: '200px'
+			});
+		}			
+	);
+});
+
